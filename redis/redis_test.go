7961aaa272e50e058a71e16ad8bc6b51ab1ba868
Alif Rachmawadi <arch@subosito.com>
2017-06-07 11:08:56 +0700

    added redis-cluster tests

--- redis/redis_test.go
+++ redis/redis_test.go
@@ -2,6 +2,7 @@ package redis_test
 
 import (
 	"os"
+	"strings"
 	"testing"
 	"time"
 
@@ -13,18 +14,126 @@ import (
 	redisc "gopkg.in/redis.v3"
 )
 
-type RedisSuite struct {
+type Provider interface {
+	redis.Provider
+	Del(keys ...string) *redisc.IntCmd
+	Set(key string, value interface{}, expiration time.Duration) *redisc.StatusCmd
+	TTL(key string) *redisc.DurationCmd
+}
+
+type CommonSuite struct {
 	suite.Suite
-	client *redisc.Client
+	client Provider
 	c      *redis.Redis
 }
 
-func (suite *RedisSuite) SetupSuite() {
+func (suite *CommonSuite) SetupSuite() {
 	gotenv.Load("../env.sample")
 }
 
-func (suite *RedisSuite) TearDownSuite() {
+func (suite *CommonSuite) TearDownSuite() {
 	os.Clearenv()
+}
+
+func (suite *CommonSuite) TearDownTest() {
+	suite.client.Del("foo")
+}
+
+func (suite *CommonSuite) TestRead() {
+	suite.loadFixtures()
+
+	b, err := suite.c.Read("foo")
+	assert.Nil(suite.T(), err)
+	assert.Equal(suite.T(), []byte(`{"foo":"bar"}`), b)
+}
+
+func (suite *CommonSuite) TestReadMap() {
+	suite.loadFixtures()
+
+	m, err := suite.c.ReadMap("foo")
+	assert.Nil(suite.T(), err)
+	assert.Equal(suite.T(), map[string]interface{}{"foo": "bar"}, m)
+}
+
+func (suite *CommonSuite) TestReadMap_invalidCache() {
+	suite.loadFixtures()
+
+	m, err := suite.c.ReadMap("baz")
+	assert.NotNil(suite.T(), err)
+	assert.Nil(suite.T(), m)
+}
+
+func (suite *CommonSuite) TestReadMap_unknownCache() {
+	suite.loadFixtures()
+
+	m, err := suite.c.ReadMap("boo")
+	assert.NotNil(suite.T(), err)
+	assert.Nil(suite.T(), m)
+}
+
+func (suite *CommonSuite) TestReadMulti() {
+	suite.loadFixtures()
+
+	keys := []string{
+		"{x}.foo",
+		"{x}.boo",
+		"{x}.fox",
+	}
+
+	m, err := suite.c.ReadMulti(keys)
+	assert.Nil(suite.T(), err)
+	assert.Len(suite.T(), m, 2)
+
+	for _, key := range keys {
+		switch key {
+		case "{x}.boo":
+			assert.Nil(suite.T(), m[key])
+		case "{x}.foo":
+			assert.Equal(suite.T(), []byte(`{"foo":"bar"}`), m[key])
+		case "{x}.fox":
+			assert.Equal(suite.T(), []byte(`{"fox":"baz"}`), m[key])
+		}
+	}
+}
+
+func (suite *CommonSuite) TestIncr() {
+	n, err := suite.c.Incr("foo")
+	assert.Nil(suite.T(), err)
+	assert.Equal(suite.T(), int64(1), n)
+}
+
+func (suite *CommonSuite) TestExpire() {
+	err := suite.client.Set("foo", "bar", time.Minute).Err()
+	assert.Nil(suite.T(), err)
+
+	b, err := suite.c.Expire("foo", time.Hour)
+	assert.Nil(suite.T(), err)
+	assert.True(suite.T(), b)
+
+	cmd := suite.client.TTL("foo")
+	assert.Nil(suite.T(), cmd.Err())
+	assert.Equal(suite.T(), time.Hour, cmd.Val())
+}
+
+func (suite *CommonSuite) loadFixtures() {
+	data := map[string]string{
+		"foo":     `{"foo":"bar"}`,
+		"fox":     `{"fox":"baz"}`,
+		"{x}.foo": `{"foo":"bar"}`,
+		"{x}.fox": `{"fox":"baz"}`,
+		"baz":     `x`,
+	}
+
+	for k, v := range data {
+		err := suite.client.Set(k, []byte(v), time.Minute).Err()
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+type RedisSuite struct {
+	*CommonSuite
 }
 
 func (suite *RedisSuite) SetupTest() {
@@ -39,105 +148,47 @@ func (suite *RedisSuite) SetupTest() {
 	})
 }
 
-func (suite *RedisSuite) TearDownTest() {
-	suite.client.Del("foo")
-}
-
 func (suite *RedisSuite) TestName() {
 	assert.Equal(suite.T(), "Redis", suite.c.Name())
 }
 
-func (suite *RedisSuite) TestRead() {
-	suite.loadFixtures()
-
-	b, err := suite.c.Read("foo")
-	assert.Nil(suite.T(), err)
-	assert.Equal(suite.T(), []byte(`{"foo":"bar"}`), b)
+func TestRedisSuite(t *testing.T) {
+	suite.Run(t, &RedisSuite{new(CommonSuite)})
 }
 
-func (suite *RedisSuite) TestReadMap() {
-	suite.loadFixtures()
-
-	m, err := suite.c.ReadMap("foo")
-	assert.Nil(suite.T(), err)
-	assert.Equal(suite.T(), map[string]interface{}{"foo": "bar"}, m)
+type RedisClusterSuite struct {
+	*CommonSuite
 }
 
-func (suite *RedisSuite) TestReadMap_invalidCache() {
-	suite.loadFixtures()
+func (suite *RedisClusterSuite) SetupTest() {
+	addrs := strings.Split(os.Getenv("REDIS_CLUSTER_ADDRS"), ",")
 
-	m, err := suite.c.ReadMap("baz")
-	assert.NotNil(suite.T(), err)
-	assert.Nil(suite.T(), m)
+	suite.client = redisc.NewClusterClient(&redisc.ClusterOptions{
+		Addrs: addrs,
+	})
+
+	suite.c = redis.New(&redis.Option{
+		Addrs: addrs,
+	})
 }
 
-func (suite *RedisSuite) TestReadMap_unknownCache() {
-	suite.loadFixtures()
-
-	m, err := suite.c.ReadMap("boo")
-	assert.NotNil(suite.T(), err)
-	assert.Nil(suite.T(), m)
+func (suite *RedisClusterSuite) TestName() {
+	assert.Equal(suite.T(), "Redis Cluster", suite.c.Name())
 }
 
-func (suite *RedisSuite) TestReadMulti() {
+func (suite *RedisClusterSuite) TestReadMulti_CROSSSLOT() {
 	suite.loadFixtures()
 
 	keys := []string{
 		"foo",
-		"boo",
-		"fox",
+		"{x}.fox",
 	}
 
 	m, err := suite.c.ReadMulti(keys)
-	assert.Nil(suite.T(), err)
-	assert.Len(suite.T(), m, 2)
-
-	for _, key := range keys {
-		switch key {
-		case "boo":
-			assert.Nil(suite.T(), m[key])
-		case "foo":
-			assert.Equal(suite.T(), []byte(`{"foo":"bar"}`), m[key])
-		case "fox":
-			assert.Equal(suite.T(), []byte(`{"fox":"baz"}`), m[key])
-		}
-	}
+	assert.Contains(suite.T(), err.Error(), "CROSSSLOT")
+	assert.Nil(suite.T(), m)
 }
 
-func (suite *RedisSuite) TestIncr() {
-	n, err := suite.c.Incr("foo")
-	assert.Nil(suite.T(), err)
-	assert.Equal(suite.T(), int64(1), n)
-}
-
-func (suite *RedisSuite) TestExpire() {
-	err := suite.client.Set("foo", "bar", time.Minute).Err()
-	assert.Nil(suite.T(), err)
-
-	b, err := suite.c.Expire("foo", time.Hour)
-	assert.Nil(suite.T(), err)
-	assert.True(suite.T(), b)
-
-	cmd := suite.client.TTL("foo")
-	assert.Nil(suite.T(), cmd.Err())
-	assert.Equal(suite.T(), time.Hour, cmd.Val())
-}
-
-func TestRedisSuite(t *testing.T) {
-	suite.Run(t, new(RedisSuite))
-}
-
-func (suite *RedisSuite) loadFixtures() {
-	data := map[string]string{
-		"foo": `{"foo":"bar"}`,
-		"fox": `{"fox":"baz"}`,
-		"baz": `x`,
-	}
-
-	for k, v := range data {
-		err := suite.client.Set(k, []byte(v), time.Minute).Err()
-		if err != nil {
-			panic(err)
-		}
-	}
+func TestRedisClusterSuite(t *testing.T) {
+	suite.Run(t, &RedisClusterSuite{new(CommonSuite)})
 }
