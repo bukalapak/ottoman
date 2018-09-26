16f608ea2617c3ee1ed4f07b7388229bbd7fbe4a
Alif Rachmawadi <subosito@bukalapak.com>
2018-09-26 16:02:34 +0700

    added cache remote provider

---
+++ cache/remote_provider.go
@@ -0,0 +1,139 @@
+package cache
+
+import (
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	multierror "github.com/hashicorp/go-multierror"
+	"github.com/pkg/errors"
+)
+
+// Fetcher is the interface for getting cache key from cache engine as well as to remote backend
+type Fetcher interface {
+	Fetch(key string, r *http.Request) (body []byte, statusCode int, err error)
+	FetchMulti(keys []string, r *http.Request) (bodies map[string][]byte, statusCodes map[string]int, err error)
+}
+
+// Resolver is the interface for resolving cache key to http request and cache router
+type Resolver interface {
+	Resolve(key string, r *http.Request) (*http.Request, error)
+	ResolveRequest(r *http.Request) (*http.Request, error)
+}
+
+// RemoteProvider enhances Provider with remote functionalities.
+type RemoteProvider interface {
+	Provider
+	Fetcher
+}
+
+type RemoteOption struct {
+	Transport http.RoundTripper
+	Timeout   time.Duration
+}
+
+func (n RemoteOption) httpClient() *http.Client {
+	return &http.Client{
+		Transport: n.httpTransport(),
+		Timeout:   n.httpTimeout(),
+	}
+}
+
+func (n RemoteOption) httpTransport() http.RoundTripper {
+	if n.Transport == nil {
+		return http.DefaultTransport
+	}
+
+	return n.Transport
+}
+
+func (n RemoteOption) httpTimeout() time.Duration {
+	if n.Timeout == 0 {
+		return 30 * time.Second
+	}
+
+	return n.Timeout
+}
+
+type remoteProvider struct {
+	Provider
+	resolver Resolver
+	option   RemoteOption
+}
+
+func NewRemoteProvider(p Provider, r Resolver, opt RemoteOption) RemoteProvider {
+	return &remoteProvider{
+		Provider: p,
+		resolver: r,
+		option:   opt,
+	}
+}
+
+func (p *remoteProvider) Fetch(key string, r *http.Request) ([]byte, int, error) {
+	req, err := p.resolver.Resolve(p.Normalize(key), r)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return p.fetchRequest(req)
+}
+
+func (p *remoteProvider) fetchRequest(r *http.Request) ([]byte, int, error) {
+	c := p.option.httpClient()
+
+	resp, err := c.Do(r)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, resp.StatusCode, errors.New("invalid http status: " + resp.Status)
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	return b, resp.StatusCode, err
+}
+
+func (p *remoteProvider) FetchMulti(keys []string, r *http.Request) (map[string][]byte, map[string]int, error) {
+	ks := p.NormalizeMulti(keys)
+
+	mb := make(map[string][]byte)
+	mn := make(map[string]int)
+
+	ec := make(chan error)
+	bc := make(chan map[string]map[int][]byte)
+
+	for _, k := range ks {
+		go func(key string) {
+			z, n, err := p.Fetch(key, r)
+			if err != nil {
+				ec <- errors.Wrap(err, key)
+			} else {
+				bc <- map[string]map[int][]byte{
+					key: {
+						n: z,
+					},
+				}
+			}
+		}(k)
+	}
+
+	var mrr *multierror.Error
+
+	for i := 0; i < len(ks); i++ {
+		select {
+		case kb := <-bc:
+			for k, v := range kb {
+				for n, b := range v {
+					mb[k] = b
+					mn[k] = n
+				}
+			}
+		case err := <-ec:
+			mrr = multierror.Append(mrr, err)
+		}
+	}
+
+	return mb, mn, mrr.ErrorOrNil()
+}
---
+++ cache/remote_provider_test.go
@@ -0,0 +1,119 @@
+package cache_test
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/bukalapak/ottoman/cache"
+	httpclone "github.com/bukalapak/ottoman/http/clone"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRemoteProvider(t *testing.T) {
+	h1 := newRemoteServer()
+	defer h1.Close()
+
+	c1 := cache.NewProvider(newSample(), "zzz")
+	q1 := cache.NewRemoteProvider(c1, newResolver(), cache.RemoteOption{})
+
+	t.Run("Fetch", func(t *testing.T) {
+		r, _ := http.NewRequest("GET", h1.URL, nil)
+		b, n, err := q1.Fetch("zoo", r)
+
+		assert.Nil(t, err)
+		assert.Equal(t, []byte(`{"zoo":"zac"}`), b)
+		assert.Equal(t, http.StatusOK, n)
+	})
+
+	t.Run("Fetch (unknown key)", func(t *testing.T) {
+		r, _ := http.NewRequest("GET", h1.URL, nil)
+		b, n, err := q1.Fetch("unknown", r)
+
+		assert.NotNil(t, err)
+		assert.Nil(t, b)
+		assert.Zero(t, n)
+	})
+
+	t.Run("Fetch (backend failure)", func(t *testing.T) {
+		r, _ := http.NewRequest("GET", h1.URL, nil)
+		b, n, err := q1.Fetch("bad", r)
+
+		assert.NotNil(t, err)
+		assert.Nil(t, b)
+		assert.Equal(t, http.StatusInternalServerError, n)
+	})
+
+	t.Run("Fetch (network failure)", func(t *testing.T) {
+		q2 := cache.NewRemoteProvider(c1, newResolver(), cache.RemoteOption{
+			Transport: &failureTransport{},
+			Timeout:   60 * time.Second,
+		})
+
+		r, _ := http.NewRequest("GET", h1.URL, nil)
+		b, n, err := q2.Fetch("zoo", r)
+
+		assert.NotNil(t, err)
+		assert.Zero(t, n)
+		assert.Nil(t, b)
+	})
+
+	t.Run("FetchMulti", func(t *testing.T) {
+		r, _ := http.NewRequest("GET", h1.URL, nil)
+		mb, mn, err := q1.FetchMulti([]string{"zoo", "unknown"}, r)
+
+		assert.Contains(t, err.Error(), "zzz:unknown: unknown cache")
+		assert.Len(t, mb, 1)
+		assert.Equal(t, []byte(`{"zoo":"zac"}`), mb["zzz:zoo"])
+		assert.Equal(t, http.StatusOK, mn["zzz:zoo"])
+	})
+}
+
+type resolver struct{}
+
+func newResolver() *resolver {
+	return &resolver{}
+}
+
+func (v *resolver) Resolve(key string, r *http.Request) (*http.Request, error) {
+	req, _ := v.ResolveRequest(r)
+
+	keys := map[string]string{
+		"zzz:bad": "/bad",
+		"zzz:zoo": "/zoo",
+	}
+
+	if v, ok := keys[key]; ok {
+		req.URL.Path = v
+	} else {
+		return nil, errors.New("unknown cache")
+	}
+
+	return req, nil
+}
+
+func (v *resolver) ResolveRequest(r *http.Request) (*http.Request, error) {
+	return httpclone.Request(r), nil
+}
+
+func newRemoteServer() *httptest.Server {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/zoo":
+			io.WriteString(w, `{"zoo":"zac"}`)
+		case "/bad":
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}
+
+	return httptest.NewServer(http.HandlerFunc(fn))
+}
+
+type failureTransport struct{}
+
+func (t *failureTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	return nil, errors.New("Connection failure")
+}
