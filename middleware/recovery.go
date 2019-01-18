f7887686a3256912ac6b7697ae5bb8a3c21e0146
Alif Rachmawadi <arch@subosito.com>
2019-01-18 16:48:14 +0700

    log panic message

--- middleware/recovery.go
+++ middleware/recovery.go
@@ -24,7 +24,7 @@ func (v *Recovery) Handler(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if rec := recover(); rec != nil {
-				v.Logger.Error("ottoman:middleware/recovery", string(debug.Stack()))
+				v.Logger.Error(rec.(string), string(debug.Stack()))
 				v.agent.Notify(rec, debug.Stack())
 				w.WriteHeader(http.StatusInternalServerError)
 			}
--- middleware/recovery_test.go
+++ middleware/recovery_test.go
@@ -17,6 +17,8 @@ func TestRecovery(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/", nil)
 	rec := httptest.NewRecorder()
 	cov := middleware.NewRecovery(NewAgent(t))
+	cov.Logger = NewLogger(t)
+
 	cov.Handler(http.HandlerFunc(fn)).ServeHTTP(rec, req)
 }
 
@@ -31,4 +33,17 @@ func NewAgent(t *testing.T) *Agent {
 func (a *Agent) Notify(err interface{}, stack []byte) {
 	assert.Equal(a.t, "!!!", err)
 	assert.NotEmpty(a.t, stack)
+}
+
+type Logger struct {
+	t *testing.T
+}
+
+func NewLogger(t *testing.T) *Logger {
+	return &Logger{t: t}
+}
+
+func (l *Logger) Error(msg string, stack string) {
+	assert.Equal(l.t, "!!!", msg)
+	assert.NotEmpty(l.t, stack)
 }
