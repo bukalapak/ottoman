cea6ccc15b1c5d18b82a075c5b6d8637e163a652
Alif Rachmawadi <subosito@bukalapak.com>
2019-04-26 00:25:46 +0700

    auto-adjust import path

--- scripts/x-http-updater.sh
+++ scripts/x-http-updater.sh
@@ -6,3 +6,6 @@ curl "https://golang.org/src/net/http/httptest/recorder.go?m=text" | sed "s/http
 curl "https://golang.org/src/net/http/httptest/recorder_test.go?m=text" | sed "s/httptest/http/g" > $DIR/recorder_test.go
 curl "https://raw.githubusercontent.com/urfave/negroni/master/response_writer.go" | sed "s/negroni/http/g" > $DIR/response_writer.go
 curl "https://raw.githubusercontent.com/urfave/negroni/master/response_writer_test.go" | sed "s/negroni/http/g" > $DIR/response_writer_test.go
+
+# adjust import path
+sed -i '' 's|internal/x/net/http/httpguts|golang.org/x/net/http/httpguts|' $DIR/recorder.go
