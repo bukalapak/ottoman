7e6deb1b4a227f32c77a6b15a3f7e4fd5c55fd27
Moch Lutfi <moch.lutfi@bukalapak.com>
2019-07-25 11:01:43 +0700

    move dummy tracker to test package

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -1,7 +1,6 @@
 package tracker
 
 import (
-	"encoding/json"
 	"errors"
 )
 
@@ -13,17 +12,4 @@ var (
 // Tracker is an interface for sending tracking data to the monitoring server
 type Tracker interface {
 	Track(data interface{}) ([]byte, error)
-}
-
-// Dummy is dummy client for testing using tracker without send them to real monitoring service
-type Dummy struct{}
-
-// Track is implementation Tracker interface to send payload to monitoring server
-// for this method just marshal and return result of marshalling
-func (d *Dummy) Track(payload interface{}) ([]byte, error) {
-	b, err := json.Marshal(payload)
-	if err != nil {
-		return nil, err
-	}
-	return b, nil
 }
--- tracker/tracker_test.go
+++ tracker/tracker_test.go
@@ -1,11 +1,29 @@
 package tracker_test
 
 import (
+	"encoding/json"
 	"testing"
 
-	"github.com/bukalapak/ottoman/tracker"
 	"github.com/google/go-cmp/cmp"
 )
+
+// dummy is dummy client for testing using tracker without send them to real monitoring service
+type dummy struct{}
+
+// Track is implementation Tracker interface to send payload to monitoring server
+// for this method just marshal and return result of marshalling
+func (d *dummy) Track(payload interface{}) ([]byte, error) {
+	b, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+// NewNullTracker is a function to create new dummy tracker
+func NewNullTracker() *dummy {
+	return &dummy{}
+}
 
 type dummyPayload struct {
 	Name string
@@ -26,7 +44,7 @@ func TestDummy_Track(t *testing.T) {
 	}
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
-			d := &tracker.Dummy{}
+			d := NewNullTracker()
 			got, err := d.Track(tt.args.payload)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Dummy.Track() error = %v, wantErr %v", err, tt.wantErr)
