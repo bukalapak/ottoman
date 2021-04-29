79ed0827e6cf843c4d3a27d71fbfa107b3ec3eec
agriardyan <aagriard@gmail.com>
2021-04-29 15:12:11 +0700

    adding table driven test

--- encoding/json/number_test.go
+++ encoding/json/number_test.go
@@ -3,116 +3,113 @@ package json_test
 import (
 	"testing"
 
+	bjson "encoding/json"
+
 	"github.com/bukalapak/ottoman/encoding/json"
 	"github.com/stretchr/testify/assert"
 )
 
+type testCase struct {
+	test               string
+	shouldErrUnmarshal bool
+	strVal             string
+	shouldErrInt       bool
+	intVal             int64
+	floatVal           float64
+	shouldErrFloat     bool
+}
+
+func runTest(t *testing.T, tc testCase, shouldUseOttomanCoder bool) {
+
+	var err error
+	var n json.Number
+
+	if shouldUseOttomanCoder {
+		err = json.Unmarshal([]byte(tc.test), &n)
+	} else {
+		err = n.UnmarshalJSON([]byte(tc.test))
+	}
+
+	if tc.shouldErrUnmarshal {
+		assert.Error(t, err)
+		return
+	}
+
+	var errM error
+	var byteArr []byte
+	if shouldUseOttomanCoder {
+		byteArr, errM = json.Marshal(n)
+	} else {
+		byteArr, errM = bjson.Marshal(n)
+	}
+
+	assert.Nil(t, errM)
+
+	if tc.test == "" || tc.test == "null" { // special condition, will be converted to empty string
+		assert.Equal(t, "", string(byteArr))
+	} else {
+		assert.Equal(t, tc.test, string(byteArr))
+	}
+
+	assert.Equal(t, tc.strVal, n.Number.String())
+
+	actualInt, err := n.Number.Int64()
+	if tc.shouldErrInt {
+		assert.Error(t, err)
+	} else {
+		assert.Nil(t, err)
+		assert.Equal(t, tc.intVal, actualInt)
+	}
+
+	actualFloat, err := n.Number.Float64()
+	if tc.shouldErrFloat {
+		assert.Error(t, err)
+	} else {
+		assert.Nil(t, err)
+		assert.Equal(t, tc.floatVal, actualFloat)
+	}
+
+}
+
+func runTestCases(t *testing.T, tcs []testCase, shouldUseOttomanCoder bool) {
+	for _, test := range tcs {
+		runTest(t, test, shouldUseOttomanCoder)
+	}
+}
+
 func TestNumber_invalid(t *testing.T) {
-	var m json.Number
 
-	x := "lorem"
-	err := m.UnmarshalJSON([]byte(x))
-	assert.NotNil(t, err)
+	var testCases = []testCase{
+		{test: "lorem", shouldErrUnmarshal: true},
+		{test: "1L", shouldErrUnmarshal: true},
+		{test: "12.1012.01", shouldErrUnmarshal: true},
+	}
 
-	x = "1L"
-	err = m.UnmarshalJSON([]byte(x))
-	assert.NotNil(t, err)
+	runTestCases(t, testCases, false)
+	runTestCases(t, testCases, true) // with ottoman decoder
 
-	x = "12.1012.01"
-	err = m.UnmarshalJSON([]byte(x))
-	assert.NotNil(t, err)
 }
 
 func TestNumber_empty_string(t *testing.T) {
-	var m json.Number
 
-	x := ""
-	err := m.UnmarshalJSON([]byte(x))
-	assert.Nil(t, err)
-	assert.Equal(t, "", m.String())
+	var testCases = []testCase{
+		{test: "", shouldErrUnmarshal: false, shouldErrInt: true, shouldErrFloat: true},
+		{test: "null", shouldErrUnmarshal: false, shouldErrInt: true, shouldErrFloat: true},
+	}
 
-	_, err = m.Int64()
-	assert.NotNil(t, err)
+	runTestCases(t, testCases, false)
+	runTestCases(t, testCases, true) // with ottoman decoder
 
-	b, err := m.MarshalJSON()
-	assert.Nil(t, err)
-	assert.Equal(t, x, string(b))
-
-	x = "null"
-	err = m.UnmarshalJSON([]byte(x))
-	assert.Nil(t, err)
-	assert.Equal(t, "", m.String())
-
-	_, err = m.Int64()
-	assert.Error(t, err)
-
-	b, err = m.MarshalJSON()
-	assert.Nil(t, err)
-	assert.Equal(t, "", string(b))
-
-	err = m.UnmarshalJSON([]byte(nil))
-	assert.Nil(t, err)
-	assert.Equal(t, "", m.String())
-
-	_, err = m.Int64()
-	assert.Error(t, err)
-
-}
-
-func TestNumber_null_string(t *testing.T) {
-	var m json.Number
-
-	x := "null"
-	err := m.UnmarshalJSON([]byte(x))
-	assert.Nil(t, err)
-
-	_, err = m.Int64()
-	assert.Error(t, err)
-
-	b, err := m.MarshalJSON()
-	assert.Nil(t, err)
-	assert.Equal(t, "", string(b))
 }
 
 func TestNumber_number_string(t *testing.T) {
-	var m json.Number
 
-	x := "3"
-	err := m.UnmarshalJSON([]byte(x))
-	assert.Nil(t, err)
+	var testCases = []testCase{
+		{test: "3", shouldErrUnmarshal: false, strVal: "3", shouldErrInt: false, intVal: 3, shouldErrFloat: false, floatVal: 3},
+		{test: "1", shouldErrUnmarshal: false, strVal: "1", shouldErrInt: false, intVal: 1, shouldErrFloat: false, floatVal: 1},
+		{test: "3.6", shouldErrUnmarshal: false, strVal: "3.6", shouldErrInt: true, shouldErrFloat: false, floatVal: 3.6},
+	}
 
-	v, err := m.Int64()
-	assert.Nil(t, err)
-	assert.Equal(t, int64(3), v)
-
-	b, err := m.MarshalJSON()
-	assert.Nil(t, err)
-	assert.Equal(t, x, string(b))
-
-	x = "1"
-	err = m.UnmarshalJSON([]byte(x))
-	assert.Nil(t, err)
-
-	v, err = m.Int64()
-	assert.Nil(t, err)
-	assert.Equal(t, int64(1), v)
-
-	b, err = m.MarshalJSON()
-	assert.Nil(t, err)
-	assert.Equal(t, x, string(b))
-
-	x = "3.6"
-	err = m.UnmarshalJSON([]byte(x))
-	assert.Nil(t, err)
-
-	v, err = m.Int64()
-	assert.Error(t, err)
-
-	u, err := m.Float64()
-	assert.Nil(t, err)
-	assert.Equal(t, float64(3.6), u)
-
-	b, err = m.MarshalJSON()
-	assert.Nil(t, err)
-	assert.Equal(t, x, string(b))
+	runTestCases(t, testCases, false)
+	runTestCases(t, testCases, true) // with ottoman decoder
 }
