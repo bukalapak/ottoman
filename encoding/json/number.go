31d10e36adcdd81b75d5123fd7dd8827df3b8b89
agriardyan <aagriard@gmail.com>
2021-04-30 15:39:58 +0700

    remove unnecessary codes

--- encoding/json/number.go
+++ encoding/json/number.go
@@ -3,27 +3,12 @@ package json
 import (
 	"encoding/json"
 	bjson "encoding/json"
-	"strconv"
 )
 
 type Number bjson.Number
 
-// String returns the literal text of the number.
-func (v Number) String() string { return string(v) }
-
-// Float64 returns the number as a float64.
-func (v Number) Float64() (float64, error) {
-	return strconv.ParseFloat(string(v), 64)
-}
-
-// Int64 returns the number as an int64.
-func (v Number) Int64() (int64, error) {
-	return strconv.ParseInt(string(v), 10, 64)
-}
-
 func (v *Number) UnmarshalJSON(b []byte) error {
-	strB := string(b)
-	if strB == `""` || strB == "null" {
+	if string(b) == `""` {
 		*v = Number("")
 		return nil
 	}
@@ -39,18 +24,6 @@ func (v *Number) UnmarshalJSON(b []byte) error {
 }
 
 func (v *Number) MarshalJSON() ([]byte, error) {
-	if v.String() == "" {
-		return json.Marshal(0)
-	}
-
-	i, err := v.Int64()
-	if err != nil {
-		f, err := v.Float64()
-		if err != nil {
-			return nil, err
-		}
-		return json.Marshal(f)
-	}
-
-	return json.Marshal(i)
+	n := bjson.Number(*v)
+	return json.Marshal(n)
 }
