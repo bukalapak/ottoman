cb0973435571b97efa4d080ab38525a7eff383bb
agriardyan <aagriard@gmail.com>
2021-05-03 15:28:42 +0700

    add String, Int64 and Float64 methods for type Number

--- encoding/json/number.go
+++ encoding/json/number.go
@@ -3,9 +3,23 @@ package json
 import (
 	"encoding/json"
 	bjson "encoding/json"
+	"strconv"
 )
 
 type Number bjson.Number
+
+// String returns the literal text of the number.
+func (n Number) String() string { return string(n) }
+
+// Float64 returns the number as a float64.
+func (n Number) Float64() (float64, error) {
+	return strconv.ParseFloat(string(n), 64)
+}
+
+// Int64 returns the number as an int64.
+func (n Number) Int64() (int64, error) {
+	return strconv.ParseInt(string(n), 10, 64)
+}
 
 func (v *Number) UnmarshalJSON(b []byte) error {
 	if string(b) == `""` {
